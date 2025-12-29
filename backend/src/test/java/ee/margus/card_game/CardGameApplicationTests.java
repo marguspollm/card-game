ec482cf4a03a7efa8e00a8f8c5a90596ae8379ee
Margus <98420540+marguspollm@users.noreply.github.com>
2025-12-29 19:15:10 +0200

    Init

---
+++ backend/src/test/java/ee/margus/card_game/CardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.margus.card_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
